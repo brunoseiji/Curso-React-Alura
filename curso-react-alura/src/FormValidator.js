7238006888a1ea0bfe3b61562037f207454e3679
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2019-10-07 00:44:33 -0300

    Validação no botão do form

---
+++ curso-react-alura/src/FormValidator.js
@@ -0,0 +1,24 @@
+import validador from 'validator';
+
+class FormValidator {
+
+    constructor(validacao) {
+        this.validacao = validacao;
+    }
+
+    valida(state) {
+        const campoValor = state[this.validacao.campo.toString()];
+        const metodoValidacao = validador[this.validacao.metodo];
+
+        if(metodoValidacao(campoValor, [], state) === true) {
+            console.log('FORM INVÁLIDO');
+            return false;
+        } else {
+            console.log('FORM VÁLIDO');
+            return true;
+        }
+    }
+
+}
+
+export default FormValidator;
